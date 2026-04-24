25a0237aba221d803d3830579046b611444c0a9c
Diego Ortiz <73130246+museortizmedia@users.noreply.github.com>
2026-04-24 13:12:12 -0500

    Feat: Stack instalation
    * Add Vue3
    * Add TypeScript
    * Add pinia
    * Use Vue Router
    * Add TailwindCSS
    * Add Axios

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p class="bg-red-500">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
